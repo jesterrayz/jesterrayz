1763f94db0aa4519f14e1899f0be0ed4db05a014
jesterrayz <139028530+jesterrayz@users.noreply.github.com>
2023-08-04 17:07:40 +0800

    asterisktest.py

--- asterisktest.py
+++ asterisktest.py
@@ -1,4 +1,12 @@
+--// just test , //--
+
 asTerisk = int(input('Enter number of line: '))
 2
 for i in range(1, asTerisk+1):
     print('*' * i)
+
+--//output will be 
+Enter number of line : 2 
+
+*
+** //--
